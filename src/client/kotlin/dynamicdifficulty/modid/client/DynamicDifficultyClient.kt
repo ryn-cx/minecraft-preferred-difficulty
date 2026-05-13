01cb6eaaa2ad36ebe96f5019ef9855f97d0a4d5e
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-05-13 07:52:45 -0700

    first commit

---
+++ src/client/kotlin/dynamicdifficulty/modid/client/DynamicDifficultyClient.kt
@@ -0,0 +1,9 @@
+package dynamicdifficulty.modid.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object DynamicDifficultyClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
