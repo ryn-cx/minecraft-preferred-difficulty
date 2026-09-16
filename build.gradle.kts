614343d72944c0ded3c8021cd7648a846673e73b
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-09-15 18:38:15 -0700

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
 	id("net.fabricmc.fabric-loom")
 	`maven-publish`
-	id("org.jetbrains.kotlin.jvm") version "2.4.0"
+	id("org.jetbrains.kotlin.jvm") version "2.4.20"
 }
 
 version = providers.gradleProperty("mod_version").get()
